b1fe278544a27843a485f3e6e323bd549bbc75ef
SergikPinsk <fedukini@inbox.ru>
2022-04-14 14:52:26 +0300

    add new Functional App

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,80 @@
-import React, { useState } from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
-import { Button } from './Components/Button.tsx/Button';
-import { Tablo } from './Tablo/Tablo';
+import {Button} from './Components/Button.tsx/Button';
+import {Tablo} from './Components/Tablo/Tablo';
+import {TabloSettings} from "./Components/TabloSettings/TabloSettings";
 
 function App() {
 
-  let[num,setNum]=useState(0)
-  const ChangeNumber = () => {
-    setNum(++num);
-  }
-  
-  const ResetButton = () => {
-    setNum(0)
-  } 
+    const [valueMax, setValueMax] = useState("")
+    const [valueMin, setValueMin] = useState("")
+    const [lokalMax, setLokalMax] = useState('')
+    const [lokalMin, setLokalMin] = useState('')
+    const [disabled, setDisabled] = useState(false)
+    const [error, setError] = useState('Братан Так делать нельзя ')
+    const [num, setNum] = useState(0)
+    const resultMax = +valueMax
+    const resultMin = +valueMin
 
-  return (
-    <div className="App">
-        <Tablo num={num}/>
-        <Button changeNumber={ChangeNumber} 
-        resetButton={ResetButton} 
-        num={num}/>
-    </div>
-  );
+   const  onClickHandler = () => {
+        setDisabled(true)
+        setValueMax(lokalMax)
+        setValueMin(lokalMin)
+        setNum(+lokalMin)
+
+    }
+    const onChangeHandlerMax = (event: ChangeEvent<HTMLInputElement>) => {
+        setLokalMax(event.currentTarget.value)
+        setDisabled(false)
+
+    }
+    const onChangeHandlerMin = (event: ChangeEvent<HTMLInputElement>) => {
+        setLokalMin(event.currentTarget.value)
+        setDisabled(false)
+        debugger
+    }
+    const ChangeNumber = () => {
+        setNum(num + 1);
+    }
+    const ResetButton = () => {
+        setNum(resultMin)
+    }
+
+    return (
+        <div className="App">
+            <div className="Counter">
+                <Tablo
+                    num={num}
+                    valueMax={valueMax}
+                    error={error}
+                    lokalMax={lokalMax}
+                    lokalMin={lokalMin}
+
+                />
+                <Button changeNumber={ChangeNumber}
+                        resetButton={ResetButton}
+                        num={num}
+                        valueMax={resultMax}
+                        valueMin={resultMin}
+
+                />
+            </div>
+            <div className="Settings">
+                <TabloSettings
+                    valueMax={valueMax}
+                    setValueMax={setValueMax}
+                    setValueMin={setValueMin}
+                    onClick={onClickHandler}
+                    onChangeHandlerMax={onChangeHandlerMax}
+                    onChangeHandlerMin={onChangeHandlerMin}
+                    resultMax={resultMax}
+                    lokalMax={lokalMax}
+                    disabled={disabled}
+                />
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
--- src/Components/Button.tsx/Button.tsx
+++ src/Components/Button.tsx/Button.tsx
@@ -5,13 +5,15 @@ type ButtonPropsType = {
     changeNumber: ()=>void,
     resetButton: ()=> void,
     num: number
+    valueMax:number
+    valueMin: number
 }
 
 export const Button = (props: ButtonPropsType) => {
     return (
         <div className="click">
-            <button disabled={props.num >= 5 ? true : false } onClick={()=> props.changeNumber()}>inc</button>
-            <button disabled={props.num === 0 ? true : false} onClick={()=> props.resetButton()}>reset</button>
+            <button disabled={props.num >= props.valueMax ? true : false } onClick={()=> props.changeNumber()}>inc</button>
+            <button disabled={props.num === props.valueMin ? true : false} onClick={()=> props.resetButton()}>reset</button>
         </div>
     )
 }
